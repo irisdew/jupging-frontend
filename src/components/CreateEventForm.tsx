b73054bbb2d2cba97d095fd84aee286e155fe98f
irisdew <ycleardew@gmail.com>
2023-03-30 00:34:21 +0900

    :lipstick: add design on CreateEventForm

--- src/components/CreateEventForm.tsx
+++ src/components/CreateEventForm.tsx
@@ -13,91 +13,107 @@ import {
   Text,
   useColorModeValue,
   Link,
+  Select,
+  Textarea,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 
 export default function CreateEventForm() {
   const [showPassword, setShowPassword] = useState(false);
+  const titleInput = useRef<HTMLInputElement>(null);
 
   return (
-    // <Flex
-    //   minH={'100vh'}
-    //   align={'center'}
-    //   justify={'center'}
-    //   bg={useColorModeValue('gray.50', 'gray.800')}
-    // >
-    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-      <Stack align={'center'}>
-        <Heading fontSize={'4xl'} textAlign={'center'}>
-          회원가입
-        </Heading>
-        {/* <Text fontSize={'lg'} color={'gray.600'}>
+    <Flex minH={'70vh'} align={'center'} justify={'center'}>
+      <Stack spacing={8} mx={'auto'} maxW={'4xl'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'3xl'} textAlign={'center'}>
+            모임 만들기
+          </Heading>
+          {/* <Text fontSize={'lg'} color={'gray.600'}>
           to enjoy all of our cool features ✌️
         </Text> */}
-      </Stack>
-      <Box
-        rounded={'lg'}
-        bg={useColorModeValue('white', 'gray.700')}
-        boxShadow={'lg'}
-        p={8}
-      >
-        <Stack spacing={4}>
-          <HStack>
-            <Box>
-              <FormControl id="firstName" isRequired>
-                <FormLabel>First Name</FormLabel>
-                <Input type="text" />
-              </FormControl>
-            </Box>
-            <Box>
-              <FormControl id="lastName">
-                <FormLabel>Last Name</FormLabel>
-                <Input type="text" />
-              </FormControl>
-            </Box>
-          </HStack>
-          <FormControl id="email" isRequired>
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl id="password" isRequired>
-            <FormLabel>Password</FormLabel>
-            <InputGroup>
-              <Input type={showPassword ? 'text' : 'password'} />
-              <InputRightElement h={'full'}>
-                <Button
-                  variant={'ghost'}
-                  onClick={() =>
-                    setShowPassword((showPassword) => !showPassword)
-                  }
-                >
-                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-          <Stack spacing={10} pt={2}>
-            <Button
-              loadingText="Submitting"
-              size="lg"
-              bg={'blue.400'}
-              color={'white'}
-              _hover={{
-                bg: 'blue.500',
-              }}
-            >
-              Sign up
-            </Button>
-          </Stack>
-          <Stack pt={6}>
-            <Text align={'center'}>
-              Already a user? <Link color={'blue.400'}>Login</Link>
-            </Text>
-          </Stack>
         </Stack>
-      </Box>
-    </Stack>
-    // </Flex>
+        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} p={8}>
+          <Stack spacing={4}>
+            <FormControl id="title">
+              <Input
+                borderRadius={13}
+                focusBorderColor={'#8CC63F'}
+                type="text"
+                placeholder="제목을 입력하세요"
+                ref={titleInput}
+              />
+            </FormControl>
+            <HStack>
+              <Box>
+                <FormControl id="date" isRequired>
+                  <Input
+                    borderRadius={13}
+                    type="date"
+                    focusBorderColor={'#8CC63F'}
+                    placeholder="날짜"
+                    ref={titleInput}
+                  />
+                </FormControl>
+              </Box>
+              <Box>
+                <Select
+                  borderRadius={13}
+                  focusBorderColor={'#8CC63F'}
+                  placeholder="장소 선택"
+                  minW={400}
+                >
+                  <option value="option1">반포한강공원</option>
+                  <option value="option2">망원한강공원</option>
+                  <option value="option3">이촌한강공원</option>
+                </Select>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <Input
+                    borderRadius={13}
+                    type="number"
+                    placeholder="참여인원"
+                    ref={titleInput}
+                  />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <Textarea
+                minH={150}
+                borderRadius={13}
+                fontSize="md"
+                focusBorderColor={'#8CC63F'}
+                placeholder="정확한 출발지와 출발시간을 꼭 기입해주세요!"
+              />
+            </FormControl>
+            <Flex gap={3} justify={'flex-end'}>
+              <Button
+                bg={'gray.500'}
+                color={'white'}
+                _hover={{
+                  bg: 'gray.300',
+                }}
+                borderRadius={13}
+              >
+                취소
+              </Button>
+              <Button
+                bg={'#8CC63F'}
+                color={'white'}
+                _hover={{
+                  bg: '#BAEB7A',
+                }}
+                borderRadius={13}
+              >
+                등록
+              </Button>
+            </Flex>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
   );
 }
