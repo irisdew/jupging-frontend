80f837c24e048ac150bc685b49db2fe263ea5213
irisdew <ycleardew@gmail.com>
2023-03-29 23:04:18 +0900

    :sparkles: create 나의 모임 관리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import {
   Grid,
+  GridItem,
   Container,
   Heading,
   Stack,
@@ -8,6 +9,7 @@ import {
   Icon,
   IconProps,
   useMediaQuery,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import Banner from '../components/Banner';
 import EventCard from '../components/EventCard';
@@ -19,6 +21,57 @@ export default function Content() {
     <Container maxW={'8xl'} p={0}>
       <Banner />
       <Stack px={20} py={10}>
+        <Heading fontSize={'2xl'} mb={5}>
+          나의 모임 관리
+        </Heading>
+        <Grid
+          templateAreas={`"header header nav"
+                  "main footer nav"`}
+          gridTemplateRows={'repeat(2, 1fr)'}
+          gridTemplateColumns={'2fr 2fr 1fr'}
+          h="300px"
+          gap={5}
+          fontWeight="bold"
+        >
+          <GridItem
+            bg={useColorModeValue('white', 'gray.900')}
+            boxShadow="lg"
+            rounded="xl"
+            px={5}
+            area={'header'}
+          >
+            Heading
+          </GridItem>
+          <GridItem
+            bg={useColorModeValue('white', 'gray.900')}
+            boxShadow={'xl'}
+            rounded="xl"
+            pl="2"
+            area={'nav'}
+          >
+            Nav
+          </GridItem>
+          <GridItem
+            bg={useColorModeValue('white', 'gray.900')}
+            boxShadow={'xl'}
+            rounded="xl"
+            pl="2"
+            area={'main'}
+          >
+            Main
+          </GridItem>
+          <GridItem
+            bg={useColorModeValue('white', 'gray.900')}
+            boxShadow={'xl'}
+            rounded="xl"
+            pl="2"
+            area={'footer'}
+          >
+            Footer
+          </GridItem>
+        </Grid>
+      </Stack>
+      <Stack px={20} py={5}>
         <Heading fontSize={'2xl'}>모임 둘러보기</Heading>
         {!isMobileWeb ? (
           <Grid templateColumns="repeat(3, 1fr)" gap={10}>
