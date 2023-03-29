541fee86c2fcce016f8cd0dd86dc9a3c40eb88d4
irisdew <ycleardew@gmail.com>
2023-03-29 21:07:35 +0900

    :sparkles: add design and state to LoginCard

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import {
   Flex,
   Box,
@@ -15,45 +15,48 @@ import {
 } from '@chakra-ui/react';
 
 export default function LoginCard() {
-  const [userId, setUserId] = useState();
-  const [password, setPassword] = useState();
+  const [inputs, setInputs] = useState<any>();
+
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+
+  const onClickLoginButton = () => {
+    setInputs({
+      email: emailInput.current?.value,
+      password: passwordInput.current?.value,
+    });
+  };
 
   return (
-    <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}
-    >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Sign in to your account</Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+    <Flex minH={'70vh'} align={'center'} justify={'center'}>
+      <Stack spacing={8} mx={'auto'} w={400} maxW={'lg'} py={12}>
+        <Stack align={'left'}>
+          <Heading fontSize={'3xl'}>이메일 로그인</Heading>
+          <Text fontSize={'md'} color={'gray.600'}>
+            가입하신 이메일 주소로 로그인하세요.
           </Text>
         </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}
-        >
+        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')}>
           <Stack spacing={4}>
             <FormControl id="userId">
-              <FormLabel>아이디</FormLabel>
               <Input
                 type="userId"
-                onChange={(e) => setUserId(e.target.value)}
+                placeholder="이메일"
+                borderRadius={13}
+                focusBorderColor={'#8CC63F'}
+                ref={emailInput}
               />
             </FormControl>
             <FormControl id="password">
-              <FormLabel>비밀번호</FormLabel>
               <Input
                 type="password"
-                onChange={(e) => setPassword(e.target.value)}
+                placeholder="비밀번호"
+                borderRadius={13}
+                focusBorderColor={'#8CC63F'}
+                ref={passwordInput}
               />
             </FormControl>
-            <Stack spacing={10}>
+            <Stack spacing={5}>
               <Stack
                 direction={{ base: 'column', sm: 'row' }}
                 align={'start'}
@@ -62,11 +65,13 @@ export default function LoginCard() {
                 {/* <Checkbox>Remember me</Checkbox> */}
               </Stack>
               <Button
-                bg={'blue.400'}
+                bg={'#8CC63F'}
                 color={'white'}
                 _hover={{
-                  bg: 'blue.500',
+                  bg: '#BAEB7A',
                 }}
+                borderRadius={13}
+                onClick={onClickLoginButton}
               >
                 로그인
               </Button>
