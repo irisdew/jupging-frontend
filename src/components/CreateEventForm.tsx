c59ae44daff54044bb04e1db4b0f5ff430ba9ea5
irisdew <ycleardew@gmail.com>
2023-03-30 22:12:38 +0900

    :sparkles: update CreateEventForm

--- src/components/CreateEventForm.tsx
+++ src/components/CreateEventForm.tsx
@@ -12,108 +12,166 @@ import {
   Heading,
   Text,
   useColorModeValue,
+  useDisclosure,
   Link,
   Select,
   Textarea,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
 } from '@chakra-ui/react';
 import { useState, useRef } from 'react';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { placeData } from '../constant/constant';
 
 export default function CreateEventForm() {
-  const [showPassword, setShowPassword] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [inputs, setInputs] = useState<any>();
+
   const titleInput = useRef<HTMLInputElement>(null);
+  const dateInput = useRef<HTMLInputElement>(null);
+  const placeInput = useRef<HTMLSelectElement>(null);
+  const peopleInput = useRef<HTMLInputElement>(null);
+  const detailInput = useRef<HTMLTextAreaElement>(null);
+
+  const onClickSubmitButton = () => {
+    setInputs({
+      title: titleInput.current?.value,
+      date: dateInput.current?.value,
+      place: placeInput.current?.value,
+      people: peopleInput.current?.value,
+      detail: detailInput.current?.value,
+    });
+    // post api
+    // alert
+    onOpen();
+  };
+  const onClickCancelButton = () => {};
 
   return (
-    <Flex minH={'70vh'} align={'center'} justify={'center'}>
-      <Stack spacing={8} mx={'auto'} maxW={'4xl'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'3xl'} textAlign={'center'}>
-            모임 만들기
-          </Heading>
-          {/* <Text fontSize={'lg'} color={'gray.600'}>
-          to enjoy all of our cool features ✌️
-        </Text> */}
-        </Stack>
-        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} p={8}>
-          <Stack spacing={4}>
-            <FormControl id="title">
-              <Input
-                borderRadius={13}
-                focusBorderColor={'#8CC63F'}
-                type="text"
-                placeholder="제목을 입력하세요"
-                ref={titleInput}
-              />
-            </FormControl>
-            <HStack>
-              <Box>
-                <FormControl id="date" isRequired>
-                  <Input
-                    borderRadius={13}
-                    type="date"
-                    focusBorderColor={'#8CC63F'}
-                    placeholder="날짜"
-                    ref={titleInput}
-                  />
-                </FormControl>
-              </Box>
-              <Box>
-                <Select
+    <>
+      <Flex minH={'70vh'} align={'center'} justify={'center'}>
+        <Stack spacing={8} mx={'auto'} maxW={'4xl'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'3xl'} textAlign={'center'}>
+              모임 만들기
+            </Heading>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            pb={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="title" isRequired>
+                <Input
                   borderRadius={13}
                   focusBorderColor={'#8CC63F'}
-                  placeholder="장소 선택"
-                  minW={400}
-                >
-                  <option value="option1">반포한강공원</option>
-                  <option value="option2">망원한강공원</option>
-                  <option value="option3">이촌한강공원</option>
-                </Select>
-              </Box>
-              <Box>
-                <FormControl id="lastName">
-                  <Input
+                  type="text"
+                  placeholder="제목을 입력하세요"
+                  ref={titleInput}
+                />
+              </FormControl>
+              <HStack>
+                <Box>
+                  <FormControl id="date" isRequired>
+                    <Input
+                      borderRadius={13}
+                      type="date"
+                      focusBorderColor={'#8CC63F'}
+                      placeholder="날짜"
+                      ref={dateInput}
+                    />
+                  </FormControl>
+                </Box>
+                <Box>
+                  <Select
                     borderRadius={13}
-                    type="number"
-                    placeholder="참여인원"
-                    ref={titleInput}
-                  />
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id="email" isRequired>
-              <Textarea
-                minH={150}
-                borderRadius={13}
-                fontSize="md"
-                focusBorderColor={'#8CC63F'}
-                placeholder="정확한 출발지와 출발시간을 꼭 기입해주세요!"
-              />
-            </FormControl>
-            <Flex gap={3} justify={'flex-end'}>
-              <Button
-                bg={'gray.500'}
-                color={'white'}
-                _hover={{
-                  bg: 'gray.300',
-                }}
-                borderRadius={13}
-              >
-                취소
-              </Button>
-              <Button
-                bg={'#8CC63F'}
-                color={'white'}
-                _hover={{
-                  bg: '#BAEB7A',
-                }}
-                borderRadius={13}
-              >
-                등록
-              </Button>
-            </Flex>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+                    focusBorderColor={'#8CC63F'}
+                    placeholder="장소 선택"
+                    minW={400}
+                    ref={placeInput}
+                  >
+                    {placeData.map((place) => (
+                      <option value={place.name}>{place.name}</option>
+                    ))}
+                  </Select>
+                </Box>
+                <Box>
+                  <FormControl id="people">
+                    <Input
+                      borderRadius={13}
+                      type="number"
+                      placeholder="참여인원"
+                      ref={peopleInput}
+                    />
+                  </FormControl>
+                </Box>
+              </HStack>
+              <FormControl id="detail" isRequired>
+                <Textarea
+                  minH={150}
+                  borderRadius={13}
+                  fontSize="md"
+                  focusBorderColor={'#8CC63F'}
+                  placeholder="정확한 출발지와 출발시간을 꼭 기입해주세요!"
+                  ref={detailInput}
+                />
+              </FormControl>
+              <Flex gap={3} justify={'flex-end'}>
+                <Button
+                  bg={'gray.500'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'gray.300',
+                  }}
+                  borderRadius={13}
+                  onClick={onClickCancelButton}
+                >
+                  취소
+                </Button>
+                <Button
+                  bg={'#8CC63F'}
+                  color={'white'}
+                  _hover={{
+                    bg: '#BAEB7A',
+                  }}
+                  borderRadius={13}
+                  onClick={onClickSubmitButton}
+                >
+                  등록
+                </Button>
+              </Flex>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalBody>안내메시지입니다!</ModalBody>
+          <ModalFooter>
+            <Button
+              bg={'#8CC63F'}
+              color={'white'}
+              _hover={{
+                bg: '#BAEB7A',
+              }}
+              mr={3}
+              borderRadius={13}
+              onClick={onClose}
+            >
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
---
+++ src/constant/constant.ts
@@ -0,0 +1,23 @@
+export const placeData = [
+  { name: '뚝섬한강공원', image: './뚝섬한강공원.png' },
+  {
+    name: '반포한강공원',
+    image: './반포한강공원.png',
+  },
+  {
+    name: '여의도한강공원',
+    image: './여의도한강공원.png',
+  },
+  {
+    name: '이촌한강공원',
+    image: './이촌한강공원.png',
+  },
+  {
+    name: '잠실한강공원',
+    image: './잠실한강공원.png',
+  },
+  {
+    name: '잠원한강공원',
+    image: './잠원한강공원.png',
+  },
+];
